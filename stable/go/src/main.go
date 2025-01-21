a2c92ab3aedb6f9a79a164a39c8d36dc2da6b478
cazzano <cazzano.agent.47@gmail.com>
2025-01-21 14:27:49 +0500

    Important

--- stable/go/src/main.go
+++ stable/go/src/main.go
@@ -7,17 +7,17 @@ import (
 )
 
 func main() {
-   if len(os.Args) < 2 {
-   	fmt.Println("Usage: main <command>")
-   	return
-   }
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: main <command>")
+		return
+	}
 
-   command := os.Args[1]
+	command := os.Args[1]
 
-   switch command {
-   case "init":
-   	runVenv() // Call the runVenv function from venv.go
-   default:
-   	fmt.Println("Unknown command:", command)
-   }
+	switch command {
+	case "vnv": // Changed from "init" to "vnv"
+		runVenv() // Call the runVenv function from venv.go
+	default:
+		fmt.Println("Unknown command:", command)
+	}
 }
