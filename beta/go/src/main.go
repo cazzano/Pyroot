02330ed8bab03cc96fc7feb65c049e903702366e
cazzano <cazzano.agent.47@gmail.com>
2025-01-30 13:58:57 +0500

    Important

--- beta/go/src/main.go
+++ beta/go/src/main.go
@@ -15,7 +15,24 @@ func main() {
 	command := os.Args[1]
 
 	switch command {
-	case "vnv": // Changed from "init" to "vnv"
+	case "new":
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: main new <folderName>")
+			return
+		}
+		folderName := os.Args[2]
+		if err := handleNew(folderName); err != nil {
+			fmt.Println("Error:", err)
+		}
+	case "-v":
+		DisplayVersion()
+	case "-h":
+		DisplayHelp()
+	case "init":
+		if err := handleInit(); err != nil {
+			fmt.Println("Error:", err)
+		}
+	case "vnv":
 		runVenv() // Call the runVenv function from venv.go
 	default:
 		fmt.Println("Unknown command:", command)
